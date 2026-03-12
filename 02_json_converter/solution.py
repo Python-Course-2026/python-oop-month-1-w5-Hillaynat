1f1cdb477676ca9482f0a0d4052adb640a3e7eab
Mustafin Denis <mustafindenis1611@gmail.com>
2026-03-13 01:33:52 +0300

    3 4 5 решены

--- 02_json_converter/solution.py
+++ 02_json_converter/solution.py
@@ -1,3 +1,9 @@
-# Напиши решение здесь
-import os
+import csv
 import json
+def convert(users.csv,json_file):
+    with open(users.csv) as users.csv:
+        reader = csv.DictReader(users_csv)
+        headers = users_csv.readline().split(',').strip()
+        with open(users.json, 'w') as json_file:
+            json.dump(reader, users, ensure_ascii=False)
+
--- 03_error_handling/solution.py
+++ 03_error_handling/solution.py
@@ -1,3 +1,15 @@
 # Напиши решение здесь
 import os
 import json
+class FileHandler:
+    def file_read(self):
+        try:
+            with open('data.txt') as data_file:
+                data = data_file.read()
+            with open('log.txt', 'w') as log_file:
+                log_file.write(data)
+        except FileNotFoundError:
+            with open('log.txt', 'w') as log_file:
+                log_file.write('ERROR')
+handler = FileHandler()
+handler.file_read()
